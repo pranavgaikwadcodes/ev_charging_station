6054c9c9e05ff35cdeb9be972e0fb861b13fa71f
Pranav Gaikwad <74352611+pranavgaikwadcodes@users.noreply.github.com>
2024-01-17 19:12:44 +0530

    Add lib directory

---
+++ lib/features/screens/bookslot/bookslot.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookSlot extends StatelessWidget {
+  const BookSlot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
